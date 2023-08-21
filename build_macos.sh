56e9724116c7ccb9ae1f19bf48994b3c08c9b6d9
NOYatsenko <45733411+noyatsenko@users.noreply.github.com>
2023-08-21 06:44:37 +0300

    Fix comments

--- build_macos.sh
+++ build_macos.sh
@@ -14,7 +14,9 @@ fi
 
 ### 1. Prepare environment
 
-brew install curl #Mojave compatibility
+#Mojave compatibility
+brew install curl
+
 brew install cmake pkg-config sdl12-compat sdl_image sdl_gfx sdl_mixer yaml-cpp doxygen
 git clone https://github.com/MeridianOXC/OpenXcom.git
 mkdir OpenXcom/build && cd OpenXcom/build
