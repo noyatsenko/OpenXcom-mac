9e314380ef2f261d9f993695d408811ba78aec3b
NOYatsenko <45733411+noyatsenko@users.noreply.github.com>
2023-10-29 17:33:55 +0300

    Update comments

--- deploy_macos.sh
+++ deploy_macos.sh
@@ -1,6 +1,6 @@
-#x86_64 on arm (God bless Rozetta)
-#softwareupdate --install-rosetta --agree-to-license
-#arch -x86_64 zsh
+### x86_64 on arm
+# softwareupdate --install-rosetta --agree-to-license
+# arch -x86_64 zsh
 
 ### 0. Install developer software
 echo "Installing HomeBrew & CommandLineTools"
@@ -14,7 +14,7 @@ fi
 
 ### 1. Prepare environment
 
-#Mojave compatibility
+# Mojave compatibility
 brew install curl
 
 brew install cmake pkg-config sdl12-compat sdl_image sdl_gfx sdl_mixer yaml-cpp doxygen
@@ -23,34 +23,28 @@ cd ~
 git clone https://github.com/MeridianOXC/OpenXcom.git
 mkdir OpenXcom/build && cd OpenXcom/build
 cmake .. -DCMAKE_BUILD_TYPE=Release 
-#-DCMAKE_OSX_DEPLOYMENT_TARGET=12
+# -DCMAKE_OSX_DEPLOYMENT_TARGET=12
 
-### 2. Link HomeBrew libs in OpenXcom/cmake/modules/PostprocessBundle.cmake 
-
-#FIND='fixup_bundle("${BUNDLE_PATH}" "${BUNDLE_LIBS}" "")'
-#REPLACE='fixup_bundle("${BUNDLE_PATH}" "${BUNDLE_LIBS}" "'$(brew --prefix)'/lib")'
-#sed -i '' "s,$FIND,$REPLACE," ../cmake/modules/PostprocessBundle.cmake 
-
-### 3. Copy SDL2 to bundle
+### 2. Copy SDL2 to bundle
 mkdir -p openxcom.app/Contents/Frameworks/
 libSDL2=`brew info SDL2 | grep Cellar | awk '{print $1}'`
 cp $libSDL2/lib/libSDL2-2.0.0.dylib openxcom.app/Contents/Frameworks
 
-### 4. Make and install
+### 3. Make and install
 
 make -j4
-#fix SDL distribute
+# fix SDL distribute
 sudo xattr -cr openxcom.app
 codesign --force --deep --sign - openxcom.app
 mv /Applications/openxcom.app /Applications/openxcom_old.app 
 mv openxcom.app /Applications
 
-### 5. Clean environment
-### sources
+### 4. Clean environment
+# sources
 cd ../..
 rm -rf ./OpenXcom
 
-### remove developer software
+# remove developer software
 read -q "REPLY?Remove HomeBrew and free 1Gb? (y/n) "
 read -p "Remove HomeBrew and free 1Gb? (y/n)" -n 1 -r
 echo
